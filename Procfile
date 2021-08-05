2ea86e5e0927cfecb4285765b27be9522ef27967
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-08-05 17:03:18 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m alakanandabot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE alakananda bot 
+:: Enables virtual env mode and then starts Evil
+env\scripts\activate.bat && py -m alakanandabott
