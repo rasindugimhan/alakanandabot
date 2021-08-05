9368bf143aebde9eb25ff157917ac53f7e536651
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-08-05 22:46:30 +0530

    Update mongo.py

--- alakanandabot/mongo.py
+++ alakanandabot/mongo.py
@@ -1,7 +1,8 @@
-#alakananda
-
+#Kiitu
+import asyncio
 import sys
 
+from motor import motor_asyncio
 from alakanandabot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
@@ -10,12 +11,12 @@ from alakanandabot.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "EVIL"
+MONGO_DB = "DaisyX"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["alakanandabot"]
 try:
