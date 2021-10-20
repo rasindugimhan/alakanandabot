97f0001b4a850053ba047570f88e9440b1c5dbfe
Rasindu Gimhan <83225696+rasindugimhan@users.noreply.github.com>
2021-10-20 12:15:24 +0530

    Update mongo.py

--- alakanandabot/mongo.py
+++ alakanandabot/mongo.py
@@ -1,24 +1,25 @@
-#HuntingBots
 import asyncio
 import sys
 
 from motor import motor_asyncio
-from alakanandabot import MONGO_DB_URI 
+from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
+
+from alakanandabot.utils.logger import log
 from alakanandabot.conf import get_int_key, get_str_key
 
+MONGO_URI = get_str_key("MONGO_DB_URI")
+MONGO_PORT = get_int_key("MONGO_PORT")
+MONGO_DB = get_str_key("MONGO_DB")
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+# Init MongoDB
+mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["alakanandabot"]
+
+engine = AIOEngine(motor, MONGO_DB)
+
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
